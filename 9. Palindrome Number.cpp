309e6e1603c469a66ba944a42c655dfcc7d2f74f
Rafael Isaac Cano Guitton <rafael.cano@ucsp.edu.pe>
2022-11-27 11:40:31 -0500

    9. Palindrome Number

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int divisor = 1;
+        while (x / divisor >= 10)
+            divisor *= 10;
+        while (x != 0)
+        {
+            int leading = x / divisor;
+            int trailing = x % 10;
+            if (leading != trailing) 
+                return false;
+            x = (x % divisor) / 10;
+            divisor = divisor / 100;
+        }
+        return true;
+    }
+};
